d5d4b180d6967ada3a0471507874244e38eee1c5
MasterKrepta <brianj.pajak@gmail.com>
2017-11-12 20:44:28 -0500

    Tentacle Swing Effect
    Chained together and ready for visuals and testing of block/attack pattern for boss

--- Assets/$Scripts/StatusChangeScripts/Tentacle.cs
+++ Assets/$Scripts/StatusChangeScripts/Tentacle.cs
@@ -7,6 +7,7 @@ public class Tentacle : MonoBehaviour
 {
 
     internal Rigidbody2D rb;
+    float distanceBetweenNodes = 0;
     // Use this for initialization
     void Start() {
 
@@ -17,17 +18,20 @@ public class Tentacle : MonoBehaviour
 
         for (int i = 0; i < childCount; i++) {
             Transform t = this.transform.GetChild(i);
-
+            
+            //Vector3 modPos = new Vector3(this.transform.position.x, distanceBetweenNodes, 0);
+            
+            //t.position = modPos;
+            //distanceBetweenNodes -=  0.5f; 
             if (t.gameObject.GetComponent<HingeJoint2D>() == null) {
                 t.gameObject.AddComponent<HingeJoint2D>();
             }
-                
             HingeJoint2D hinge = t.gameObject.GetComponent<HingeJoint2D>();
 
             hinge.connectedBody = i == 0 ? this.rb : this.transform.GetChild(i - 1).GetComponent<Rigidbody2D>();
-
-            hinge.useMotor = true;
-            hinge.enableCollision = true;
+            
+           // hinge.useMotor = true;
+           hinge.enableCollision = false;
         }
     }
 }
